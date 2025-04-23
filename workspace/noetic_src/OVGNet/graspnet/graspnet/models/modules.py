ee7001e6ea4d907d49446052e06900a2359d9f83
FariqJavier <fariqjavier@gmail.com>
2025-04-23 22:29:26 +0700

    fix relative path

--- workspace/noetic_src/OVGNet/graspnet/graspnet/models/modules.py
+++ workspace/noetic_src/OVGNet/graspnet/graspnet/models/modules.py
@@ -8,15 +8,9 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = os.path.dirname(BASE_DIR)
-sys.path.append(ROOT_DIR)
-sys.path.append(os.path.join(ROOT_DIR, 'pointnet2'))
-sys.path.append(os.path.join(ROOT_DIR, 'utils'))
-
-from pointnet2 import pytorch_utils as pt_utils
-from pointnet2.pointnet2_utils import CylinderQueryAndGroup
-from utils.loss_utils import generate_grasp_views, batch_viewpoint_params_to_matrix
+from ..pointnet2 import pytorch_utils as pt_utils
+from ..pointnet2.pointnet2_utils import CylinderQueryAndGroup
+from ..utils.loss_utils import generate_grasp_views, batch_viewpoint_params_to_matrix
 
 
 class ApproachNet(nn.Module):
